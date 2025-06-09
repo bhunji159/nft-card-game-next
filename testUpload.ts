63245ee4fe38281eea6dac0746ab381670cdd11e
STARiv2r <64356856+STARiv2r@users.noreply.github.com>
2025-06-09 21:09:49 +0900

    10종류 추가

--- testUpload.ts
+++ testUpload.ts
@@ -1,13 +1,16 @@
 // testUpload.ts (루트에 생성해서 node로 실행)
-import { uploadImage, uploadMetadata } from './src/app/ipfs';
+import { uploadImage, uploadMetadata, ipfsToHttp } from "./src/app/ipfs";
 
 (async () => {
-    const imageCid = await uploadImage('./public/image.png');
-    console.log('Image CID:', imageCid);
+  const imageCid = await uploadImage("./public/성원숭.jpg");
+  console.log("Image CID:", imageCid);
 
-    // 메타데이터 JSON에 image CID를 넣고 업로드
-    const metadataCid = await uploadMetadata('./metadata/1.json');
-    console.log('Metadata CID:', metadataCid);
+  // 메타데이터 JSON에 image CID를 넣고 업로드
+  // const metadataCid = await uploadMetadata("./metadata/2.json");
+  // console.log("Metadata CID:", metadataCid);
 
-    console.log(`tokenURI: ipfs://${metadataCid}`);
+  // console.log(`tokenURI: ipfs://${metadataCid}`);
+
+  const httpUrl = ipfsToHttp(imageCid, "https://ipfs.io/ipfs/");
+  console.log("Image URL:", httpUrl);
 })();
